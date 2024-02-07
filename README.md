60983f46d8d4eff01da039cba365a303516766f6
sulo1337 <sulochan.acharya2@gmail.com>
2024-02-07 11:57:42 -0600

    feat: initial commit - working coder kubevirt provisioner

---
+++ README.md
@@ -0,0 +1,108 @@
+
+# Coder Kubevirt Template
+[![Apache 2.0 License](https://img.shields.io/badge/License-Apache%202.0-green.svg)](https://choosealicense.com/licenses/apache-2.0/)
+
+This project is a template to create reproducible dev environments in [Coder](https://github.com/coder/coder) using [Kubevirt](https://github.com/kubevirt/kubevirt) running on a Kubernetes cluster.
+
+Coder enables organizations to set up development environments in their public or private cloud infrastructure. Cloud development environments are defined with Terraform, connected through a secure high-speed Wireguard® tunnel, and are automatically shut down when not in use to save on costs. Coder gives engineering teams the flexibility to use the cloud for workloads that are most beneficial to them.
+
+KubeVirt is a virtual machine management add-on for Kubernetes. The aim is to provide a common ground for virtualization solutions on top of Kubernetes.
+
+Read below for motive.
+
+## Pre-requisites
+- A running kubernetes cluster with Kubevirt deployed. See Kubevirt [installation guide](https://kubevirt.io/user-guide/operations/installation).
+- Containerized Data Importer (CDI) should be installed in the kubernetes cluster for PVC management. See CDI [installation guide](https://kubevirt.io/user-guide/operations/containerized_data_importer/).
+- Default storage class configured in the kubernetes cluster.
+- Bare-metal kubernetes cluster preferred. For kubernetes clusters running on top of VMs, nested-virtualization support is required.
+
+#### Kubernetes API Access Pre-requisite
+If the Coder host is running outside the Kubernetes cluster (where workspace VMs will be deployed), a valid "~/.kube/config" must be present on the Coder host.
+
+If Coder host is deployed on the same Kubernetes cluster (where workspace VMs will be deployed), a service account provisioned by coder will be used for workspace deployments. 
+
+In both cases, the service-account/user(in the kubeconfig) should have bindings for following roles:
+
+| type        	| apiGroups            	| resources                 	| verbs            	| namespace                                      	|
+|-------------	|----------------------	|---------------------------	|------------------	|------------------------------------------------	|
+| clusterrole 	| apiextensions.k8s.io 	| customresourcedefinitions 	| get, list, watch 	| -                                              	|
+| clusterrole 	| kubevirt.io          	| virtualmachines           	| *                	| -                                              	|
+| clusterrole 	| cdi.kubevirt.io      	| datavolumes               	| *                	| -                                              	|
+| role        	| ""                   	| secrets                   	| *                	| namespace where workspace VMs will be deployed 	|
+
+Permission to access secret in the namespace where VMs are deployed is required to store cloud-init configs. This secret is then mount to workspace VMs as a cloud-init drive. Kubevirt only supports 2048 byte cloud-init config if set as string. To overcome this limit, Kubernetes secrets are used.
+
+## Features
+
+- Provision KVM Virtual Machines as dev workspaces
+- Persists whole root filesystem instead of just home directory
+- Persists software installs in the OS
+- Code server is enabled by default
+- SSH is configured
+- VMs can be started/stopped/restarted from Coder webapp without losing data
+- Major Linux distributions are pre-configured
+    - Ubuntu 22.04
+    - Debian 12
+    - Fedora 39
+    - Arch Linux
+    - AlmaLinux 9
+    - CentOS Stream 9
+    - Rocky Linux 9
+- Automatically downloads OS drives from cloud to create disk PVCs
+
+## Installation 
+This installation assumes you have a Coder deployment running and CLI authenticated.
+
+- Clone this repository
+```sh
+git clone https://github.com/sulo1337/coder-kubevirt-template.git && cd coder-kubevirt-template/kubevirt-provisioner
+```
+- Push the template
+```sh
+coder templates push .
+```
+
+## Defaults
+These are the default values configured in the template. These values can be changed based on the requirements
+
+- CPU - 2 cores
+- Memory - 4Gi
+- Disk Size - 16Gi
+- Network configured in masquerade mode. See [kubevirt networking documentation](https://kubevirt.io/user-guide/virtual_machines/interfaces_and_networks/) for more details
+
+## Roadmap
+- Enable sourcing disk PVC creations from another PVC. Currently disk PVC data is sourced from qcow2 cloud images of various OS. This feature will allow a cluster administrator to pre-configure bootable image of an OS as PVC and use that PVC as a source to create new VM disk.
+- Windows OS as VMs
+- VNC 
+- Multi-disk VMs 
+- Shared disks as PVC attached to VMs allowing dev workspaces to share files.
+
+## Notes
+- Management of Kubevirt is out of scope of this repository
+- VolumeSnapshot and PVC backups of VM disks is out of scope of this repository
+
+
+## Motive
+I created this template because of three reasons. 
+- To safely allow container runtimes to run inside coder workspace
+- To utilize existing Kubernetes cluster to provision workspaces capable of running native container workloads
+- To use existing bare-metal infrastructure for provisioning coder workspaces instead of using cloud providers (AWS, Azure etc)
+
+There are other benefits to using Virtual Machine vs Containers for dev environments which is out of scope for this documentation.
+
+
+Workspaces provisioned by Coder in Kubernetes are container environments in a pod. Most modern software development requires use of containerization technologies like Docker as a part of development workflow. According to [Coder Docs](https://coder.com/docs/v2/latest/templates/docker-in-workspaces), there are multiple ways to run container environments inside pods provisioned by Coder:
+- Sysbox container runtime (needs specific infrastructure configuration, and enterprise license for [more features](https://github.com/nestybox/sysbox/blob/master/docs/figures/sysbox-features.png))
+- Envbox*
+- Rootless podman*
+- Privileged docker sidecar*
+*All of these methods either will make you create a privileged pod inside your cluster or have some privileged wrapper around your workspace (except rootless podman). Limitations of using rootless podman is out of scope for this documentation.
+
+I created this template to extend Coder's functionality to provision VMs on existing Kubernetes clusters. KVM machines being used as coder workspaces unlocks these functionalities:
+- docker inside coder workspaces
+- systemd
+- mini kubernetes environments inside coder workspaces using [minikube](https://github.com/kubernetes/minikube) or [kind](https://github.com/kubernetes-sigs/kind)
+- access to raw devices (needs configuration)
+- nested virtualization inside coder workspace (needs configuration)
+- more isolation from host compared to pods
+and many more...
---
+++ kubevirt-provisioner/main.tf
@@ -0,0 +1,363 @@
+terraform {
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = "0.13.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.25.2"
+    }
+  }
+}
+
+variable "namespace" {
+  description = <<-EOF
+  Kubernetes namespace to deploy the workspace VM into
+
+  EOF
+  default     = "coder"
+}
+
+variable "use_kubeconfig" {
+  type        = bool
+  description = <<-EOF
+  Use host kubeconfig? (true/false)
+
+  Set this to false if the Coder host is itself running as a Pod on the same
+  Kubernetes cluster as you are deploying workspaces to.
+
+  Set this to true if the Coder host is running outside the Kubernetes cluster
+  for workspaces.  A valid "~/.kube/config" must be present on the Coder host.
+  EOF
+  default     = false
+}
+
+provider "coder" {
+}
+
+provider "kubernetes" {
+  config_path = var.use_kubeconfig == true ? "~/.kube/config" : null
+}
+
+data "coder_workspace" "me" {
+
+}
+
+data "coder_parameter" "os_image" {
+    name = "os_image"
+    display_name = "OS Image"
+    description = "OS Image type should your workspace use?"
+    default = "https://cloud-images.ubuntu.com/jammy/20240131/jammy-server-cloudimg-amd64-disk-kvm.img"
+    mutable = false
+    option {
+        name = "Ubuntu 22.04"
+        value = "https://cloud-images.ubuntu.com/jammy/20240131/jammy-server-cloudimg-amd64-disk-kvm.img"
+        icon = "/icon/ubuntu.svg"
+    }
+    option {
+        name = "Fedora 39"
+        value = "https://download.fedoraproject.org/pub/fedora/linux/releases/39/Cloud/x86_64/images/Fedora-Cloud-Base-39-1.5.x86_64.qcow2"
+        icon = "/icon/fedora.svg"
+    }
+    option {
+        name = "Debian 12"
+        value = "https://cloud.debian.org/images/cloud/bookworm/20240201-1644/debian-12-genericcloud-amd64-20240201-1644.qcow2"
+        icon = "/icon/debian.svg"
+    }
+    option {
+        name = "Arch Linux"
+        value = "https://geo.mirror.pkgbuild.com/images/v20240201.210938/Arch-Linux-x86_64-cloudimg.qcow2"
+        icon = "https://cdn0.iconfinder.com/data/icons/flat-round-system/512/archlinux-512.png"
+    }
+    option {
+        name = "AlmaLinux 9"
+        value = "https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-9.3-20231113.x86_64.qcow2"
+        icon = "/icon/almalinux.svg"
+    }
+    option {
+        name = "CentOS Stream 9"
+        value = "https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-9-20240129.0.x86_64.qcow2"
+        icon = "/icon/centos.svg"
+    }
+    option {
+        name = "Rocky Linux 9"
+        value = "https://download.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base-9.3-20231113.0.x86_64.qcow2"
+        icon = "/icon/rockylinux.svg"
+    }
+}
+
+data "coder_parameter" "linux_user" {
+  name        = "Linux username"
+  type        = "string"
+  description = "Username for default coder workspace linux user"
+  validation {
+    regex = "^[a-z_][a-z0-9_-]{0,31}$"
+    error = "Username must satisfy regex /^[a-z_][a-z0-9_-]{0,31}$/"
+  }
+  default = "coder"
+}
+
+data "coder_parameter" "cpu" {
+  name        = "CPU cores"
+  type        = "number"
+  description = "Number of CPU Cores"
+  icon        = "https://png.pngtree.com/png-clipart/20191122/original/pngtree-processor-icon-png-image_5165793.jpg"
+  validation {
+    min = 1
+    max = 4
+  }
+  mutable = true
+  default = 2
+  order   = 1
+}
+
+data "coder_parameter" "memory" {
+  name        = "Memory (__ MB)"
+  type        = "number"
+  description = ""
+  icon        = "https://www.vhv.rs/dpng/d/33-338595_random-access-memory-logo-hd-png-download.png"
+  validation {
+    min = 1024
+    max = 8192
+  }
+  mutable = true
+  default = 4096
+  order   = 2
+}
+
+data "coder_parameter" "disk_size" {
+  name        = "PVC (your rootfs) storage size"
+  type        = "number"
+  description = "Number of GB of storage"
+  icon        = "https://www.pngall.com/wp-content/uploads/5/Database-Storage-PNG-Clipart.png"
+  validation {
+    min       = 4
+    max       = 64
+    monotonic = "increasing"
+  }
+  mutable = false
+  default = 16
+  order   = 3
+}
+
+resource "coder_agent" "dev" {
+  count = data.coder_workspace.me.start_count
+  arch                   = "amd64"
+  auth                   = "token"
+  dir                    = "/home/${data.coder_parameter.linux_user.value}"
+  os                     = "linux"
+  startup_script_timeout = 300
+  startup_script         = <<EOT
+    set -e
+
+    # install and start code-server
+    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
+    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+  EOT
+
+  metadata {
+    key          = "cpu"
+    display_name = "CPU Usage"
+    interval     = 5
+    timeout      = 5
+    script       = "coder stat cpu"
+  }
+  metadata {
+    key          = "memory"
+    display_name = "Memory Usage"
+    interval     = 5
+    timeout      = 5
+    script       = "coder stat mem"
+  }
+  metadata {
+    key          = "disk"
+    display_name = "Disk Usage"
+    interval     = 600 # every 10 minutes
+    timeout      = 30  # df can take a while on large filesystems
+    script       = "coder stat disk --path /"
+  }
+  display_apps {
+    vscode          = true
+    vscode_insiders = false
+    web_terminal    = true
+    ssh_helper      = true
+    port_forwarding_helper = true
+  }
+}
+
+resource "coder_app" "code-server" {
+  agent_id     = try(coder_agent.dev[0].id, "")
+  slug         = "code-server-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+  display_name = "code-server"
+  icon         = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2560px-Visual_Studio_Code_1.35_icon.svg.png"
+  url          = "http://localhost:13337/?folder=/home/${data.coder_parameter.linux_user.value}"
+
+  healthcheck {
+    url       = "http://localhost:13337/healthz"
+    interval  = 3
+    threshold = 10
+  }
+}
+
+# cloud init user data and coder agent api token is stored in secret
+# secret is destroyed on stopping workspace and re-created on starting a workspace
+resource "kubernetes_secret" "cloudinit-secret" {
+  count = data.coder_workspace.me.transition == "start" ? 1 : 0
+  metadata {
+    name      = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}-cloudinit"
+    namespace = var.namespace
+  }
+  data = {
+    userdata = <<EOT
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+hostname: ${lower(data.coder_workspace.me.name)}
+users:
+- name: ${lower(data.coder_parameter.linux_user.value)}
+  passwd: $y$j9T$7rB9aUNSRJTEzm8t7qjdd1$klhw8gB81nd.6Q/T2EXu/bgiMjxPRWSHf1u8ZXloYf3
+  lock_passwd: false
+  sudo: ALL=(ALL) NOPASSWD:ALL
+  shell: /bin/bash
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+export CODER_AGENT_TOKEN=${try(coder_agent.dev[0].token, "")}
+sudo --preserve-env=CODER_AGENT_TOKEN -u ${lower(data.coder_parameter.linux_user.value)} /bin/bash -c '${try(coder_agent.dev[0].init_script, "")}' > /var/log/coder_agent.log 2>&1
+--//--
+EOT
+  }
+}
+
+resource "kubernetes_manifest" "virtualmachine" {
+  count = data.coder_workspace.me.transition == "start" ? 1 : 0
+  manifest = {
+    "apiVersion" = "kubevirt.io/v1"
+    "kind"       = "VirtualMachine"
+    "metadata" = {
+      "labels" = {
+        "kubevirt.io/vm" = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+      }
+      "name"      = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+      "namespace" = "${var.namespace}"
+    }
+    "spec" = {
+      "running" = true
+      "template" = {
+        "metadata" = {
+          "creationTimestamp" = null
+          "labels" = {
+            "kubevirt.io/vm" = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+          }
+        }
+        "spec" = {
+          "domain" = {
+            "devices" = {
+              "disks" = [
+                {
+                  "disk" = {
+                    "bus" = "virtio"
+                  }
+                  "name" = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}-rootfs"
+                },
+                {
+                  "disk" = {
+                    "bus" = "virtio"
+                  }
+                  "name" = "cloudinitdisk"
+                }
+              ]
+              "interfaces" = [
+                {
+                  "masquerade" = {}
+                  "name"       = "default"
+                },
+              ]
+            }
+            "resources" = {
+              "requests" = {
+                "cpu"    = "${data.coder_parameter.cpu.value}"
+                "memory" = "${data.coder_parameter.memory.value}M"
+              }
+            }
+          }
+          "networks" = [
+            {
+              "name" = "default"
+              "pod"  = {}
+            },
+          ]
+          "terminationGracePeriodSeconds" = 300
+          "volumes" = [
+            {
+              "dataVolume" = {
+                "name" = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}-rootfs"
+              }
+              "name" = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}-rootfs"
+            },
+            {
+              "cloudInitNoCloud" = {
+                "networkData" = <<-EOT
+                #cloud-config
+                network:
+                  version: 2
+                  ethernets:
+                    enp1s0:
+                      dhcp4: true
+                EOT
+                "secretRef" = {
+                  name = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}-cloudinit"
+                }
+              }
+              "name" = "cloudinitdisk"
+            }
+          ]
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "datavolume" {
+  manifest = {
+    "apiVersion" = "cdi.kubevirt.io/v1beta1"
+    "kind"       = "DataVolume"
+    "metadata" = {
+      "name"      = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}-rootfs"
+      "namespace" = "${var.namespace}"
+    }
+    "spec" = {
+      "pvc" = {
+        "accessModes" = [
+          "ReadWriteOnce",
+        ]
+        "resources" = {
+          "requests" = {
+            "storage" = "${data.coder_parameter.disk_size.value}G"
+          }
+        }
+      }
+      "source" = {
+        "http" = {
+          "url" = "${data.coder_parameter.os_image.value}"
+        }
+      }
+    }
+  }
+}
+
